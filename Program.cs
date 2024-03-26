3bab5e557883e82e78b40143cb8750e5d7256791
va00297223 <160072555+va00297223@users.noreply.github.com>
2024-03-26 16:07:27 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,12 +3,10 @@ using Decorator_Design.Decorator;
 
 var component = new Concrete_Class();
 
-// Create decorators
 var plain_Decorator = new PlainDecorator(component);
 var upperCase_Decorator = new UpperCaseDecorator(component);
 var color_Decorator = new ColorDecorator(component);
 
-// Use decorators
 Console.WriteLine("Initial Text: " + component.GetText());
 Console.WriteLine();
 Console.WriteLine("Plain Text: " + plain_Decorator.GetText());
